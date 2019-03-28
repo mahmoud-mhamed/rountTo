bb55c2f99ba667203fa7e1a82a1e3780fecaa3df
mahmoud <mahmoudmohamed5211997749@yahoo.com>
2019-03-28 22:18:54 +0200

    improve script

--- roundTo.js
+++ roundTo.js
@@ -2,6 +2,9 @@ function roundTo(num,fraction=2){
     num +='';
     var dot=num.indexOf('.');
     var trunc=Math.trunc(num);
+    if (trunc==0 && num < 0 && fraction!=0){
+        trunc='-'+trunc;
+    }
 
     var newNum='';
     for (var i = 0; i < fraction && i < ((num.length - (trunc+'').length) -1); i++) {
